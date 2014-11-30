799d0cf9973168cd3369773bc7570907e2141fe3
Christian Moestl <moestl@hm.edu>
2014-11-30 20:35:32 +0100

    Added JavadocType.java

--- src/main/java/edu/hm/hafner/JavadocType.java
+++ src/main/java/edu/hm/hafner/JavadocType.java
@@ -1,11 +1,27 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type JavadocType.
- *
- * @author Christian M&ouml;stl
+ * Useless class - only for test-cases.
  */
 public class JavadocType {
-    
-}
+    private final int number;
 
+    /**
+     * Creates a new instance of {@link JavadocType}.
+     *
+     * @param number
+     *            number
+     */
+    public JavadocType(final int number) {
+        this.number = number;
+    }
+
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+}
