0685444305327b7ee3b24894449035c1d32a5914
Christian Moestl <moestl@hm.edu>
2014-11-30 17:35:13 +0100

    Added EmptyBlock.java

--- src/main/java/edu/hm/hafner/EmptyBlock.java
+++ src/main/java/edu/hm/hafner/EmptyBlock.java
@@ -1,11 +1,20 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type EmptyBlock.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class EmptyBlock {
-    
-}
+    /**
+     * Creates a new instance of {@link EmptyBlock}.
+     *
+     * @param a
+     *            number
+     */
+    public EmptyBlock(final int a) {
+        if (a > 0) {
 
+        }
+    }
+}
