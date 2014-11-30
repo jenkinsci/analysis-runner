7627d9295976e0fcdf8b9f353e4f55e6f7e79faa
Christian Moestl <moestl@hm.edu>
2014-11-30 17:21:14 +0100

    Added CovariantEquals.java

--- src/main/java/edu/hm/hafner/CovariantEquals.java
+++ src/main/java/edu/hm/hafner/CovariantEquals.java
@@ -1,11 +1,35 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type CovariantEquals.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class CovariantEquals {
-    
-}
+    private int x;
+    private int y;
+    private int z;
+    private String s;
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((s == null) ? 0 : s.hashCode());
+        result = prime * result + x;
+        result = prime * result + y;
+        result = prime * result + z;
+        return result;
+    }
+
+    /**
+     * Wrong equals..
+     *
+     * @param b
+     *            b
+     * @return true or false
+     */
+    public boolean equals(final boolean b) {
+        return b;
+    }
+}
