1f91e1a7604341297382e45505465398c2b98cb9
Christian Moestl <moestl@hm.edu>
2014-11-30 19:55:13 +0100

    Added IllegalImport

--- src/main/java/edu/hm/hafner/IllegalImport.java
+++ src/main/java/edu/hm/hafner/IllegalImport.java
@@ -1,11 +1,13 @@
 package edu.hm.hafner;
 
+import java.sql.Date;
+
+
 /**
- * FIXME: Document type IllegalImport.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class IllegalImport {
-    
+    private Date date;
 }
-
