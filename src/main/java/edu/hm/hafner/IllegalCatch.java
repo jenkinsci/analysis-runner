b57e6d0bab95d798298c1aa9d328e7af0b87ead2
Christian Moestl <moestl@hm.edu>
2014-11-30 19:51:59 +0100

    Added HideUtilityClassConstructor.java and IllegalCatch.java

--- src/main/java/edu/hm/hafner/IllegalCatch.java
+++ src/main/java/edu/hm/hafner/IllegalCatch.java
@@ -1,11 +1,34 @@
 package edu.hm.hafner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 /**
- * FIXME: Document type IllegalCatch.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
-public class IllegalCatch {
-    
-}
+public final class IllegalCatch {
 
+    private IllegalCatch() {
+
+    }
+
+    /**
+     * Reads sth from a file.
+     *
+     * @param file
+     *            the file
+     */
+    public static void read(final File file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader.read();
+            reader.close();
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
