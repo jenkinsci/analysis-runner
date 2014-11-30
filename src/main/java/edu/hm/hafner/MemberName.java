d3bac61c0525a3076fdf5a6a13c5e510bdc40793
Christian Moestl <moestl@hm.edu>
2014-11-30 21:30:37 +0100

    Added Membername.java

--- src/main/java/edu/hm/hafner/MemberName.java
+++ src/main/java/edu/hm/hafner/MemberName.java
@@ -1,11 +1,43 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type MemberName.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class MemberName {
-    
-}
+    private final int Number;
+    private final String text;
 
+    /**
+     * Creates a new instance of {@link MemberName}.
+     *
+     * @param number
+     *            number
+     * @param text
+     *            text
+     */
+    public MemberName(final int number, final String text) {
+        int someNumber = 2;
+        Number = someNumber + number;
+        this.text = text;
+    }
+
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return Number;
+    }
+
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+}
