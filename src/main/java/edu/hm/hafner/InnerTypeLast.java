1c43650b6e9666143439c2d93133eb4d9d8dca9f
Christian Moestl <moestl@hm.edu>
2014-11-30 20:12:54 +0100

    Added innerTypeLast.java

--- src/main/java/edu/hm/hafner/InnerTypeLast.java
+++ src/main/java/edu/hm/hafner/InnerTypeLast.java
@@ -1,11 +1,71 @@
 package edu.hm.hafner;
 
+import java.util.Date;
+
 /**
- * FIXME: Document type InnerTypeLast.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class InnerTypeLast {
-    
-}
 
+    private final Date date;
+    private final int number;
+    private final String text;
+
+    /**
+     * Creates a new instance of {@link InnerTypeLast}.
+     *
+     * @param date
+     *            Date
+     * @param number
+     *            number
+     * @param text
+     *            Text
+     */
+    public InnerTypeLast(final Date date, final int number, final String text) {
+        this.date = date;
+        this.number = number;
+        this.text = text;
+    }
+
+    /**
+     * Returns the date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+
+    /**
+    *
+    * Inner-Class.
+    *
+    * @author Christian M&ouml;stl
+    */
+   class InnerClass {
+       void doSth() {
+           System.out.println("Hello");
+       }
+   }
+
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+}
