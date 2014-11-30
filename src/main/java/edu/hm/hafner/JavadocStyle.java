5872c12b654bb01aa29dc60b675127583a8cb85a
Christian Moestl <moestl@hm.edu>
2014-11-30 20:27:15 +0100

    Added JavadocStyle.java

--- src/main/java/edu/hm/hafner/JavadocStyle.java
+++ src/main/java/edu/hm/hafner/JavadocStyle.java
@@ -1,11 +1,15 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type JavadocStyle.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class JavadocStyle {
-    
-}
+    /**
+     * Creates a new instance of {@link JavadocStyle}
+     */
+    public JavadocStyle() {
 
+    }
+}
