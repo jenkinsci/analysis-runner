0fb4036e103029d7bbf440c49963c6ff81dafc42
Christian Moestl <moestl@hm.edu>
2014-11-30 20:05:36 +0100

    Added InnerAssignment.java

--- src/main/java/edu/hm/hafner/InnerAssignment.java
+++ src/main/java/edu/hm/hafner/InnerAssignment.java
@@ -1,11 +1,46 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type InnerAssignment.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class InnerAssignment {
-    
-}
+    private final String s;
+    private int a;
 
+    /**
+     * Creates a new instance of {@link InnerAssignment}.
+     *
+     * @param s
+     *            text
+     * @param a
+     *            number
+     */
+    public InnerAssignment(final String s, final int a) {
+        this.s = s;
+        this.a = a;
+    }
+
+    String doSth() {
+        return Integer.toString(a = 2);
+    }
+
+    /**
+     * Returns the s.
+     *
+     * @return the s
+     */
+    public String getS() {
+        return s;
+    }
+
+    /**
+     * Returns the a.
+     *
+     * @return the a
+     */
+    public int getA() {
+        return a;
+    }
+}
