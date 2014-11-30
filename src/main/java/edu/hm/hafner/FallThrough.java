db0df8a453ab995abf2ec30bbcad9469743067d7
Christian Moestl <moestl@hm.edu>
2014-11-30 19:19:08 +0100

    Added FallThrough.java

--- src/main/java/edu/hm/hafner/FallThrough.java
+++ src/main/java/edu/hm/hafner/FallThrough.java
@@ -1,11 +1,68 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type FallThrough.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class FallThrough {
-    
+    private String monat;
+
+    /**
+     * Creates a new instance of {@link DefaultComesLast}.
+     *
+     * @param month
+     *            monat
+     */
+    public FallThrough(final int month) {
+        switch (month) {
+            case 1:
+                monat = "Januar";
+            case 2:
+                monat = "Februar";
+                break;
+            case 3:
+                monat = "März";
+                break;
+            case 4:
+                monat = "April";
+                break;
+            case 5:
+                monat = "Mai";
+                break;
+            case 6:
+                monat = "Juni";
+                break;
+            case 7:
+                monat = "Juli";
+                break;
+            case 8:
+                monat = "August";
+                break;
+            case 9:
+                monat = "September";
+                break;
+            case 10:
+                monat = "Oktober";
+                break;
+            case 11:
+                monat = "November";
+                break;
+            case 12:
+                monat = "Dezember";
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Returns the monat.
+     *
+     * @return the monat
+     */
+    public String getMonat() {
+        return monat;
+    }
 }
 
