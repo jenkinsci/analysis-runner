1231a055382d1cdffb4d4cb44edf14ce5afb38d0
Christian Moestl <moestl@hm.edu>
2014-11-30 21:20:13 +0100

    Added LeftCurly

--- src/main/java/edu/hm/hafner/LeftCurly.java
+++ src/main/java/edu/hm/hafner/LeftCurly.java
@@ -1,11 +1,44 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type LeftCurly.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
-public class LeftCurly {
-    
-}
+public class LeftCurly
+{
+    private final int x;
 
+    private final int y;
+
+    /**
+     * Creates a new instance of {@link RightCurly}.
+     *
+     * @param x
+     *            number
+     * @param y
+     *            number
+     */
+    public LeftCurly(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Returns the x.
+     *
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y.
+     *
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+}
