1f1a3bbaf07a15e0b7269df7b0391366cb3703ec
Christian Moestl <moestl@hm.edu>
2014-11-30 16:11:34 +0100

    added code to ArrayTypeStyle.java

--- src/main/java/edu/hm/hafner/ArrayTypeStyle.java
+++ src/main/java/edu/hm/hafner/ArrayTypeStyle.java
@@ -1,11 +1,18 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type ArrayTypeStyle.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class ArrayTypeStyle {
-    
+    /**
+     * main.
+     *
+     * @param args
+     *            args.
+     */
+    public void x(final String args[]) {
+        System.out.println("d");
+    }
 }
-
