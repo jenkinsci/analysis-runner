b6c3abb037df49f901d7d2d2a542cd05d511f42c
Christian Moestl <moestl@hm.edu>
2014-11-30 20:42:12 +0100

    Added JavadocVariable.java

--- src/main/java/edu/hm/hafner/JavadocVariable.java
+++ src/main/java/edu/hm/hafner/JavadocVariable.java
@@ -1,11 +1,31 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type JavadocVariable.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class JavadocVariable {
-    
-}
 
+    public int number = 1;
+    private final String text;
+
+    /**
+     * Creates a new instance of {@link JavadocVariable}.
+     *
+     * @param text
+     *            Text
+     */
+    public JavadocVariable(final String text) {
+        this.text = text;
+    }
+
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+}
