bb367680aa36fde2ac7769f770924294a34be3f5
Christian Moestl <moestl@hm.edu>
2014-11-30 21:14:50 +0100

    Added JUnitTestCase.java

--- src/main/java/edu/hm/hafner/JUnitTestCase.java
+++ src/main/java/edu/hm/hafner/JUnitTestCase.java
@@ -1,11 +1,26 @@
 package edu.hm.hafner;
 
+import org.junit.Test;
+
 /**
- * FIXME: Document type JUnitTestCase.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class JUnitTestCase {
-    
-}
 
+    /**
+     * SetUp-Method.
+     */
+    @Test
+    public void setup() {
+        System.out.println("do something for starting...");
+    }
+
+    /**
+     * The JUnit-tearDown()-method.
+     */
+    public void tearDown() {
+        System.out.println("finishing....");
+    }
+}
