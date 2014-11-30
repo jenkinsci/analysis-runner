9daaaffed57de4497f15186a24f0f612e30587ef
Christian Moestl <moestl@hm.edu>
2014-11-30 21:26:52 +0100

    Added LocalFinalVariableName.java and LocalVariableName.java

--- src/main/java/edu/hm/hafner/LocalFinalVariableName.java
+++ src/main/java/edu/hm/hafner/LocalFinalVariableName.java
@@ -1,11 +1,43 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type LocalFinalVariableName.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class LocalFinalVariableName {
-    
-}
+    private final int number;
+    private final String text;
 
+    /**
+     * Creates a new instance of {@link LocalFinalVariableName}.
+     *
+     * @param number
+     *            number
+     * @param text
+     *            text
+     */
+    public LocalFinalVariableName(final int number, final String text) {
+        final int SomeNumber = 2;
+        this.number = SomeNumber + number;
+        this.text = text;
+    }
+
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+}
--- src/main/java/edu/hm/hafner/LocalVariableName.java
+++ src/main/java/edu/hm/hafner/LocalVariableName.java
@@ -1,11 +1,43 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type LocalVariableName.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class LocalVariableName {
-    
-}
+    private final int number;
+    private final String text;
 
+    /**
+     * Creates a new instance of {@link LocalVariableName}.
+     *
+     * @param number
+     *            number
+     * @param text
+     *            text
+     */
+    public LocalVariableName(final int number, final String text) {
+        int SomeNumber = 2;
+        this.number = SomeNumber + number;
+        this.text = text;
+    }
+
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+}
