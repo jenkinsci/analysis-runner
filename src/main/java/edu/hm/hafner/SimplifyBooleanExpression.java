6586adaf49c55175a0e9d31a54ad9664461ae04b
Christian Moestl <moestl@hm.edu>
2014-12-01 16:28:36 +0100

    Added SimplyfyBooleanExpression.java

--- src/main/java/edu/hm/hafner/SimplifyBooleanExpression.java
+++ src/main/java/edu/hm/hafner/SimplifyBooleanExpression.java
@@ -1,11 +1,80 @@
 package edu.hm.hafner;
 
+import java.util.Date;
+
 /**
- * FIXME: Document type SimplifyBooleanExpression.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class SimplifyBooleanExpression {
+    private Date date;
+    private int number;
+    private final String text;
     
+    /**
+     * Creates a new instance of {@link SimplifyBooleanExpression}.
+     *
+     * @param date
+     *            Date
+     * @param number
+     *            number
+     * @param text
+     *            Text
+     */
+    public SimplifyBooleanExpression(final Date date, final int number, final String text) {
+        this.date = date;
+        this.number = number;
+        this.text = text;
+    }
+    
+    /**
+     * Do sth...
+     * 
+     * @param a
+     *            number
+     * @param isTrue
+     *            boolean-value
+     */
+    public void doSth(int a, boolean isTrue) {
+        int b = 0;
+        if (isTrue == true) {
+            System.out.println("a<0");
+        }
+        if (a > 0) {
+            System.out.println(">0");
+            if (a == 1) {
+                ++b;
+                System.out.println("a=1");
+            }
+        }
+        System.out.println(b);
+    }
+    
+    /**
+     * Returns the date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+    
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+    
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
 }
-
