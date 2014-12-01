562c664d9a0beabbf9ce3b3acb3e98924af4fe81
Christian Moestl <moestl@hm.edu>
2014-12-01 13:41:13 +0100

    Added NestedTryDepth.java

--- src/main/java/edu/hm/hafner/NestedTryDepth.java
+++ src/main/java/edu/hm/hafner/NestedTryDepth.java
@@ -1,11 +1,94 @@
 package edu.hm.hafner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 /**
- * FIXME: Document type NestedTryDepth.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class NestedTryDepth {
+    private final Date date;
+    private final int number;
     
+    /**
+     * Creates a new instance of {@link NestedTryDepth}.
+     *
+     * @param date
+     *            Date
+     * @param number
+     *            number
+     */
+    public NestedTryDepth(final Date date, final int number) {
+        this.date = date;
+        this.number = number;
+    }
+    
+    /**
+     * Do sth..
+     */
+    public void doSthWithNestedTry() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("")));
+            try {
+                bufferedReader.close();
+            }
+            catch (IOException exception2) {
+                exception2.printStackTrace();
+            }
+            try {
+                bufferedReader.read();
+                try {
+                    Cipher.getInstance("AES/CBC/PKCS5Padding").doFinal();
+                }
+                catch (IllegalBlockSizeException exception) {
+                    exception.printStackTrace();
+                }
+                catch (BadPaddingException exception) {
+                    exception.printStackTrace();
+                }
+                catch (NoSuchAlgorithmException exception) {
+                    exception.printStackTrace();
+                }
+                catch (NoSuchPaddingException exception) {
+                    exception.printStackTrace();
+                }
+            }
+            catch (IOException exception1) {
+                exception1.printStackTrace();
+            }
+        }
+        catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+    
+    /**
+     * Returns the date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+    
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
 }
-
