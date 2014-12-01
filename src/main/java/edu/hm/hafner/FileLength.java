e530006af6076e3ca629a76b97bf61014bad3309
Christian Moestl <moestl@hm.edu>
2014-12-01 18:27:53 +0100

    Added FileLength.java

--- src/main/java/edu/hm/hafner/FileLength.java
+++ src/main/java/edu/hm/hafner/FileLength.java
@@ -5,7 +5,184 @@ package edu.hm.hafner;
  *
  * @author Christian M&ouml;stl
  */
-public class FileLength {
-
+public final class FileLength {
+    
+    private FileLength() {
+        
+    }
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met1(int a) {
+        int b = 1;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met2(int a) {
+        int b = 2;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met3(int a) {
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met4(int a) {
+        int b = 4;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met5(int a) {
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met6(int a) {
+        int b = 6;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met7(int a) {
+        int b = 7;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
+    
+    /**
+     * Do sth.
+     * 
+     * @param a
+     *            number
+     */
+    public static void met8(int a) {
+        int b = 8;
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+        System.out.println(a + b);
+    }
 }
-
