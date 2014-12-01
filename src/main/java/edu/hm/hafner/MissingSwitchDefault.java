663b3a1efc08da28e32c9d85b419e4be732cf21c
Christian Moestl <moestl@hm.edu>
2014-12-01 12:40:28 +0100

    Added MissingSwitchDefault.java

--- src/main/java/edu/hm/hafner/MissingSwitchDefault.java
+++ src/main/java/edu/hm/hafner/MissingSwitchDefault.java
@@ -1,11 +1,67 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type MissingSwitchDefault.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class MissingSwitchDefault {
-    
-}
+    private String monat;
 
+    /**
+     * Creates a new instance of {@link MissingSwitchDefault}.
+     *
+     * @param month
+     *            monat
+     */
+    public MissingSwitchDefault(final int month) {
+
+        switch (month) {
+            case 1:
+                monat = "Januar";
+                break;
+            case 2:
+                monat = "Februar";
+                break;
+            case 3:
+                monat = "März";
+                break;
+            case 4:
+                monat = "April";
+                break;
+            case 5:
+                monat = "Mai";
+                break;
+            case 6:
+                monat = "Juni";
+                break;
+            case 7:
+                monat = "Juli";
+                break;
+            case 8:
+                monat = "August";
+                break;
+            case 9:
+                monat = "September";
+                break;
+            case 10:
+                monat = "Oktober";
+                break;
+            case 11:
+                monat = "November";
+                break;
+            case 12:
+                monat = "Dezember";
+                break;
+        }
+    }
+
+    /**
+     * Returns the monat.
+     *
+     * @return the monat
+     */
+    public String getMonat() {
+        return monat;
+    }
+}
