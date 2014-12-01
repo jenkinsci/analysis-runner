5e11398814a86aef2ad32f1439e41eca960cee2d
Christian Moestl <moestl@hm.edu>
2014-12-01 17:06:57 +0100

    Added UnnessaryParentheses.java

--- src/main/java/edu/hm/hafner/UnnecessaryParentheses.java
+++ src/main/java/edu/hm/hafner/UnnecessaryParentheses.java
@@ -1,11 +1,78 @@
 package edu.hm.hafner;
 
+import java.util.Date;
+
 /**
- * FIXME: Document type UnnecessaryParentheses.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class UnnecessaryParentheses {
+    private Date date;
+    private int number;
+    private final String text;
     
+    /**
+     * Creates a new instance of {@link UnnecessaryParentheses}.
+     *
+     * @param date
+     *            Date
+     * @param number
+     *            number
+     * @param text
+     *            Text
+     */
+    public UnnecessaryParentheses(final Date date, final int number, final String text) {
+        this.date = date;
+        this.number = number;
+        this.text = text;
+    }
+    
+    /**
+     * Do sth...
+     * 
+     * @param a
+     *            number
+     */
+    public void doSth(int a) {
+        int b = 0;
+        if (a < 0) {
+            System.out.println("a<0");
+        }
+        if (a > 0) {
+            System.out.println(">0");
+            if ((a == 1)) {
+                ++b;
+                System.out.println("a=1");
+            }
+        }
+        System.out.println(b);
+    }
+    
+    /**
+     * Returns the date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+    
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+    
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
 }
-
