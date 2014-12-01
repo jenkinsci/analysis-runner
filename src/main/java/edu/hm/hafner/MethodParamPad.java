a91ba7393fcce806d60380ab7428421cbda7c0bf
Christian Moestl <moestl@hm.edu>
2014-12-01 12:09:19 +0100

    Added MethodParamPad.java

--- src/main/java/edu/hm/hafner/MethodParamPad.java
+++ src/main/java/edu/hm/hafner/MethodParamPad.java
@@ -1,11 +1,36 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type MethodParamPad.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class MethodParamPad {
-    
-}
+    private final int x;
 
+    /**
+     * Creates a new instance of {@link MethodParamPad}.
+     *
+     * @param x
+     *            number
+     */
+    public MethodParamPad(final int x) {
+        this.x = x;
+    }
+
+    /**
+     * Do sth.
+     */
+    public void method () {
+        System.out.println("prints sth...");
+    }
+
+    /**
+     * Returns the x.
+     *
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+}
