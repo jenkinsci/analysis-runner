e223e868bf774988cda35bf98660b4cdf200b70b
Christian Moestl <moestl@hm.edu>
2014-12-01 17:04:15 +0100

    Added TypeName.java

--- src/main/java/edu/hm/hafner/TypeName.java
+++ src/main/java/edu/hm/hafner/TypeName.java
@@ -1,11 +1,79 @@
 package edu.hm.hafner;
 
+import java.util.Date;
+
 /**
- * FIXME: Document type TypeName.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
-public class TypeName {
+public class typeName {
+    private Date date;
+    private int number;
+    private final String text;
     
+    /**
+     * Creates a new instance of {@link typeName}.
+     *
+     * @param date
+     *            Date
+     * @param number
+     *            number
+     * @param text
+     *            Text
+     */
+    public typeName(final Date date, final int number, final String text) {
+        this.date = date;
+        this.number = number;
+        this.text = text;
+    }
+    
+    /**
+     * Do sth...
+     * 
+     * @param a
+     *            number
+     * @param isTrue
+     *            boolean-value
+     */
+    public void doSth(int a, boolean isTrue) {
+        int b = 0;
+        if (isTrue) {
+            System.out.println("a<0");
+        }
+        if (a > 0) {
+            System.out.println(">0");
+            if (a == 1) {
+                ++b;
+                System.out.println("a=1");
+            }
+        }
+    }
+    
+    /**
+     * Returns the date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+    
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+    
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }  
 }
-
