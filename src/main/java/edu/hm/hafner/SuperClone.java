10b9ea986f712d5cbe8d359756428fcf2b040b21
Christian Moestl <moestl@hm.edu>
2014-12-01 16:48:08 +0100

    Added SuperClone.java

--- src/main/java/edu/hm/hafner/SuperClone.java
+++ src/main/java/edu/hm/hafner/SuperClone.java
@@ -1,11 +1,92 @@
 package edu.hm.hafner;
 
+import java.util.Date;
+
 /**
- * FIXME: Document type SuperClone.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class SuperClone {
+    private Date date;
+    private int number;
+    private final String text;
     
+    /**
+     * Creates a new instance of {@link SuperClone}.
+     *
+     * @param date
+     *            Date
+     * @param number
+     *            number
+     * @param text
+     *            Text
+     */
+    public SuperClone(final Date date, final int number, final String text) {
+        this.date = date;
+        this.number = number;
+        this.text = text;
+    }
+    
+    /**
+     * Do sth...
+     * 
+     * @param a
+     *            number
+     * @param isTrue
+     *            boolean-value
+     */
+    public void doSth(int a, boolean isTrue) {
+        int b = 0;
+        if (isTrue) {
+            System.out.println("a<0");
+        }
+        if (a > 0) {
+            System.out.println(">0");
+            if (a == 1) {
+                ++b;
+                System.out.println("a=1");
+            }
+        }
+        System.out.println(b);
+    }
+    
+    /**
+     * False clone()....
+     * 
+     * @return Object
+     * @throws CloneNotSupportedException
+     *             Throw Exception
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Object();
+    }
+    
+    /**
+     * Returns the date.
+     *
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+    
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+    
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
 }
-
