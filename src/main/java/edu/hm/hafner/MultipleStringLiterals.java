6a0bb709ed5745b8316dfe0de30825d58583882c
Christian Moestl <moestl@hm.edu>
2014-12-01 12:53:44 +0100

    Added MultipleStringLiterals.java

--- src/main/java/edu/hm/hafner/MultipleStringLiterals.java
+++ src/main/java/edu/hm/hafner/MultipleStringLiterals.java
@@ -1,11 +1,57 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type MultipleStringLiterals.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class MultipleStringLiterals {
-    
-}
+    private int a;
+    private int b;
+    private int c;
 
+    /**
+     * Prints sth...
+     */
+    public void doSth() {
+        System.out.println("Dieser String kommt mehrmals vor");
+        System.out.println("------");
+        System.out.println("Dieser String kommt mehrmals vor");
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + a;
+        result = prime * result + b;
+        result = prime * result + c;
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MultipleStringLiterals other = (MultipleStringLiterals)obj;
+        if (a != other.a) {
+            return false;
+        }
+        if (b != other.b) {
+            return false;
+        }
+        if (c != other.c) {
+            return false;
+        }
+        return true;
+    }
+
+
+}
