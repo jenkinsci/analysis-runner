3089e6cd35d14489758f3b3606d36379e1945275
Christian Moestl <moestl@hm.edu>
2014-12-01 12:57:35 +0100

    Added MultipleVariableDeclarations.java

--- src/main/java/edu/hm/hafner/MultipleVariableDeclarations.java
+++ src/main/java/edu/hm/hafner/MultipleVariableDeclarations.java
@@ -1,11 +1,55 @@
 package edu.hm.hafner;
 
 /**
- * FIXME: Document type MultipleVariableDeclarations.
+ * Useless class - only for test-cases.
  *
  * @author Christian M&ouml;stl
  */
 public class MultipleVariableDeclarations {
-    
-}
+    private final int number; private final int x;
+    private final String text;
 
+    /**
+     * Creates a new instance of {@link MultipleVariableDeclarations}.
+     *
+     * @param number
+     *            number
+     * @param text
+     *            text
+     * @param x
+     *            number
+     */
+    public MultipleVariableDeclarations(final int number, final String text, final int x) {
+        int someNumber = 2;
+        this.number = someNumber + number;
+        this.text = text;
+        this.x = x;
+    }
+
+    /**
+     * Returns the x.
+     *
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the number.
+     *
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * Returns the text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+}
